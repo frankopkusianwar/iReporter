14e7188f02c836f60bc03578298392f98914d8d2
frankopkusianwar <okirorfrank3@gmail.com>
2018-11-27 23:56:53 +0300

    [finishes #162180744] edit incident

--- api/models/ireportermodels.py
+++ api/models/ireportermodels.py
@@ -1,31 +1,41 @@
 class User:
 
-    def __init__(self, userId=int, firstName="", lastName="", 
-                otherNames="", userName="", email="", password="", 
+    def __init__(self, user_id=int, first_name="", last_name="", 
+                other_names="", user_name="", email="", password="", 
                 registered="", isAdmin="False"):
 
-        self.userId = userId
-        self.firstName = firstName
-        self.lastName = lastName
-        self.otherNames = otherNames
-        self.userName = userName
+        self.user_id = user_id
+        self.first_name = first_name
+        self.last_name = last_name
+        self.other_names = other_names
+        self.user_name = user_name
         self.email = email
         self.password = password
-        self.registered = registered
 
 users = []
 
 class Incident:
-    def __init__(self, incidentId=int, createdOn="", createdBy="", incidentType="",
-                location="", status="draft", images=[], 
+    def __init__(self, created_by="",
+                latitude="", longitude="", images=[], 
                 comment=""):
-        self.incidentId = incidentId
-        self.createdOn = createdOn
-        self.createdBy = createdBy
-        self.incidentType = incidentType
-        self.location = location
-        self.status = status
+
+        self.latitude = latitude
+        self.longitude = longitude
+        self.status = "draft"
         self.images = images
+
+class  RedFlag(Incident):
+    """docstring for  RedFlag"""
+    def __init__(self):
+        Incident.__init__(self)
+        self.red_flag_incident_type = "red-flag"
+
+class  Intervention(Incident):
+    """docstring for  RedFlag"""
+    def __init__(self):
+        Incident.__init__(self)
+        self.internention_incident_type = "intervention"
+        
 
 incidents = []    
         
--- api/views/ireporterViews.py
+++ api/views/ireporterViews.py
@@ -1,5 +1,5 @@
 from flask import Blueprint
-from api.controllers.ireportercontrollers import addUser, addIncident, getAllIncidents, searchId, incidents, deleteId
+from api.controllers.ireportercontrollers import addUser, add_red_flag, add_intervention, getAllIncidents, searchId, incidents, deleteId, edit_incident
 
 bp = Blueprint("ireporterViews", __name__, url_prefix="/api/v1")
 
@@ -8,11 +8,15 @@ def createUser():
     return addUser()
 
 @bp.route("/red-flags", methods=["POST"])
-def createIncident():
-    return addIncident()
+def create_red_flag_record():
+    return add_red_flag()
 
-@bp.route("/red-flags", methods=["GET"])
-def get_all_red_flag_records():
+@bp.route("/interventions", methods=["POST"])
+def create_intervention_record():
+    return add_intervention()
+
+@bp.route("/incidents", methods=["GET"])
+def get_all_incident_records():
     return getAllIncidents()
 
 @bp.route("/red-flags/<int:red_flag_id>", methods=["GET"])
@@ -20,8 +24,8 @@ def get_specific_redflag(red_flag_id):
     return searchId(red_flag_id, incidents)
 
 @bp.route("/red-flags/<int:red_flag_id>/location", methods=["PATCH"])
-def edit_specific_redflag_location():
-    pass
+def edit_specific_redflag_location(red_flag_id):
+    return edit_incident(red_flag_id,incidents)
 
 @bp.route("/red-flags/<int:red_flag_id>/comment", methods=["PATCH"])
 def add_comment_to_specific_redflag_record():
